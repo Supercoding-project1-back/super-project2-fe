cbce441b5382d7af360e65103ab516c76d0d4c88
cookie0215 <jina_0215@naver.com>
2024-07-08 22:27:37 +0900

    REMOVE: Header 컴포넌트 삭제

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
-import Header from './components/Header';
 import ProductDetail from './pages/ProductDetail/ProductDetail';
 import ProductUpload from './pages/ProductUpload/ProductUpload';
 
 function App() {
   return (
     <div className="App">
-      <Header />
       <ProductDetail />
       {/* <ProductUpload /> */}
     </div>
