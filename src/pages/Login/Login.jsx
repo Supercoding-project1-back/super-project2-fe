c966f78a54ec69725b714590b034df93d9309fa6
supercoding_backend <myec1235@naver.com>
2024-07-21 18:23:33 +0900

    Feat: 마이페이지 및 로그인 생성

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,10 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
+import styles from "./Login.module.scss";
+import { TextField } from "../../components/Core";
+import { useNavigate } from "react-router-dom";
 
-function Login () {
-    return(
-        <>
-        </>
-    )
+function Login() {
+  const navigate = useNavigate();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signHandler = async () => {
+    const data = { email, password };
+    try {
+      const response = await fetch("http://localhost:8080/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+      if (!response.ok) {
+        throw new Error("로그인하는데에 실패하였습니다");
+      }
+    } catch (error) {
+      alert("로그인 정보를 다시 입력해 주세요");
+    }
+  };
+
+  const joinHandler = () => {
+    navigate("/join");
+  };
+
+  return (
+    <>
+      <div className={styles["content"]}>
+        <div className={styles["card"]}>
+          <div className={styles["border"]}>
+            <div className={styles["first-section"]}>
+              <span className={styles["login-title"]}></span>
+            </div>
+            <div className={styles["input-section"]}>
+              <div className={styles["text-section"]}>
+                <TextField
+                  className={styles["loginId"]}
+                  value={email}
+                  placeholder={"이메일"}
+                  onChange={(value) => setEmail(value)}
+                />
+              </div>
+              <div className={styles["text-section"]}>
+                <TextField
+                  className={styles["password"]}
+                  value={password}
+                  placeholder={"비밀번호"}
+                  onChange={(value) => setPassword(value)}
+                />
+              </div>
+            </div>
+            <div className={styles["button-section"]}>
+              <button className={styles["login"]} onClick={signHandler}>
+                SIGN IN
+              </button>
+              <button className={styles["join"]} onClick={joinHandler}>
+                JOIN US
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Login;
